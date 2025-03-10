59889ac5eb67feec0439593a9e6e7cb7fe234b08
Pavan kumar <gujjaripavan226@gmail.com>
2025-03-10 19:33:33 +0530

    Added large dataset with Git LFS and updated src.py

---
+++ src.py
@@ -0,0 +1,8 @@
+#Importing The Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#Loading the dataset
+df=pd.read_csv("https://raw.githubusercontent.com/gujjaripavan222/Explonatory-Data-Analysis-using-Ecommerce-dataset/refs/heads/main/data.csv")
+print(df.head())
